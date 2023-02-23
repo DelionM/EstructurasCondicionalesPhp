9eef0b37b5030265aea9a8671de86704829d4b48
Brayan Giovanni Delion Martínez <103006766+DelionM@users.noreply.github.com>
2023-02-22 23:03:18 -0600

    Add files via upload

---
+++ EstructurasCondicionales/index.php
@@ -0,0 +1,15 @@
+<?php
+    echo "Estructuras condicionales If else <br>";
+    echo  $a = 10;
+    echo "<br>";
+    echo  $b = 100;
+    echo "<br>";
+
+    if($a < $b)
+        echo "el valor de a es menor que el valor de b <br> ";
+
+        elseif($a > $b)
+            echo "el valor de a es mayor a b <br>";
+    else
+        echo "el valor de a es igual a  de b <br>";
+?>
